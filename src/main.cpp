accd39308bb82c8f5e41f89e22a3099ca6b9945f
Pedro Botelho <pedrobotelho15@alu.ufc.br>
2021-02-24 23:43:33 -0300

    Main Function v3.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include "thumblator.hpp"
 
+//PARAMETER BY TERMINAL
+//argv[0] = ./thumblator
+//argv[1] = <input>
+//argv[2] = <output>
 int main(int argc, char* argv[]){ 
     
+    //PARAMETER CHECK
     if(argc > 3){
         cout << "\033[1;37mThumblator:\033[0m \033[1;31mFatal error\033[0m: too much parameters.\nAborting.\n";
         return 0;
@@ -11,19 +16,22 @@ int main(int argc, char* argv[]){
         return 0;
     }
 
+    //OPENING AND CHECKING INPUT FILE
     ifstream input_file(argv[1]);
     if(!(input_file.is_open())){
         cout << "\033[1;37mThumblator:\033[0m \033[1;31mFatal error\033[0m: file doesn't exist.\nAborting.\n";
         return 0;
     }
-
+    
+    //EXTENSION CHECK
     string input_file_name;
     input_file_name = argv[1];
     if (input_file_name.find(".in") == input_file_name.npos){
         cout << "\033[1;37mThumblator:\033[0m \033[1;31mFatal error\033[0m: invalid extension.\nAborting.\n";
         return 0;
     }
-
+    
+    //FILE TO STRING
     stringstream auxiliary_buffer;
     auxiliary_buffer << input_file.rdbuf();
     string source_code;
@@ -34,9 +42,11 @@ int main(int argc, char* argv[]){
     long int numeric_opcode = 0;
     string opcode;
 
+    //REMOVE SPACES
     while (source_code.find(' ') != source_code.npos)
         source_code.erase(source_code.find(' '), 1);
-
+    
+    //INSTRUCTION DECODE
     while (step < source_code.size()){
         while (source_code[step] != '\n' && step != source_code.size())
             opcode += source_code[step++];
@@ -47,15 +57,21 @@ int main(int argc, char* argv[]){
         step += opcode.size() + 4;
         opcode.clear();
     }
-
+    
+    //REMOVE LAST LINE FEED
     source_code.erase(source_code.size() - 1, 1);
+    
+    //OUTPUT FILE CREATION
     ofstream output_file;
     if (argc == 2)
         output_file.open(input_file_name.replace(input_file_name.find(".in"), 3, ".out"));
     else
         output_file.open(argv[2]);
+    
+    //STRING TO FILE
     output_file << source_code;
 
+    //CLOSING FILES
     output_file.close();
     input_file.close();
     return 0;
